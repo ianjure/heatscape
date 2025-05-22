8abc74046f316f4b1ae4e18dfa8178ee99f27584
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-05-22 15:08:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -289,7 +289,7 @@ with col2:
 
     col1, col2, col3 = st.columns(3)
 
-     with col1:
+    with col1:
         with st.container(border=True):
             st.markdown(f"""
                 <div style="text-align: center;">
