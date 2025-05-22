22832839e2b06e01fd51a05db75d692d89b75434
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-05-22 14:30:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ with col1:
     ).add_to(map)
     
     # DISPLAY MAP
-    map.to_streamlit(width=None, height=100, add_layer_control=False)
+    map.to_streamlit(width=None, height=1000, add_layer_control=False)
 
 with col2:
     # Add a table showing all barangays with scrolling capability
