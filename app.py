65dc5b22040f92a27054f3134ecec0dd58d4ab04
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-05-22 15:32:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -169,6 +169,7 @@ with st.sidebar:
 # APPLY MULTIPLIERS TO FEATURE VALUES
 sim_scaled = sim_data.copy()
 sim_scaled['NDBI'] *= ndbi_mult
+sim_scaled['NDBI'] = sim_scaled['NDBI'].clip(-1, 1)
 sim_scaled['nighttime_lights'] *= nlights_mult
 sim_scaled['omega_500'] *= omega_mult
 sim_scaled['cooling_capacity'] *= cooling_mult
